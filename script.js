10386ff5233cf96b0b37c7c34eb2503f21cfe2d4
Marlon Macintyre <44618584+marlon320@users.noreply.github.com>
2018-12-03 01:15:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ $(document).ready(() => {
 
 var build_home_interface = function() {
     let body = $('body');
-
+//poop
     body.empty();
     body.append('<nav class="navbar"><a class="navbar-item" id="home">Home</a><a class="navbar-item"></a></nav>');
     body.append("<section class='section header-section'><div class='container header-container'><h1 class='title'>Welcome to Flights API Project</h1><h1 class='subtitle'></h1>");
