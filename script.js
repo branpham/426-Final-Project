603ae163c9c1257c5ac5c0997186c60412420b6a
branpham <branpham@branpham-pc.wireless-1x.unc.edu>
2018-12-03 21:27:45 -0500

    searchairports unfinished

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ var root_url = "http://comp426.cs.unc.edu:3001/";
 
 $(document).ready(function(){
   // load AJAX log in information
+  searchAirports();
+  console('Searching for airport data');
   $.ajax({
     type: 'POST',
     url: root_url + 'sessions/',
@@ -27,4 +29,19 @@ var build_home_interface = function() {
     body.append();
 
 
+}
+
+var searchAirports = function() {
+	let flightcont = $('.container.flight-container');
+	$.ajax({
+    type: 'GET',
+    url: root_url + 'airports',
+    xhrFields: {withCredentials: true},
+    success: (response) => {
+    	let airport = response;
+    	let airport_name = airport.name;
+    	flightcont.append('<li class="collection-item"><a href="#">' + airport_name +'</a></li>');
+    	console.log(airport_name);
+    }
+  });
 }
