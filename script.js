740fb05fb7d4958057693cb22cd68f7b4b536021
branpham <branpham@branpham-pc.wireless-1x.unc.edu>
2018-12-03 20:36:44 -0500

    added filter search

--- script.js
+++ script.js
@@ -1,38 +1,22 @@
 var root_url = "http://comp426.cs.unc.edu:3001/";
 
-$(document).ready(() => {
-    // $('#login_btn').on('click', () => {
-	
-	
-	// let user = $('#login_user').val();
-	// let pass = $('#login_pass').val();
-
-	// console.log(user);
-	// console.log(pass);
-	
-	$.ajax({
-		   type: 'POST',
-		   url: root_url + 'sessions/',
-		   data: { "user": {
-		       "username": 'b',
-		       "password": 'password'
-		   		}	
-		   },
-		   xhrFields: {withCredentials: true},
-		   success: (response) => {
-		       if (response.status) {
-			   // build_home_interface();
-			   alert('worked');
-		       } else {
-			   $('#mesg_div').html("Login failed. Try again.");
-		       }
-		   },
-		   error: () => {
-		       alert('error');
-		   }
-		});
-	});
-// }); 
+$(document).ready(function(){
+  // load AJAX log in information
+  $.ajax({
+    type: 'POST',
+    url: root_url + 'sessions/',
+    data: { "user": {
+                    "username": 'b',
+                    "password": 'password'
+                    }
+          },
+    xhrFields: {withCredentials: true},
+    async: true,
+    success: (response) => {
+    	build_home_interface();
+      console.log('POST to sessions was a success');
+    }
+  });
 
 var build_home_interface = function() {
     let body = $('body');
@@ -44,3 +28,4 @@ var build_home_interface = function() {
 
 
 }
+
