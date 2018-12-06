62dfbc6e9ab31fd90e312f7084a9ed51927d0357
David Nguyen <david.nnguyen@yahoo.com>
2018-12-06 09:56:21 -0500

    test

--- filterlist.js
+++ filterlist.js
@@ -6,12 +6,10 @@
     function filterNames(){
       // Get value of input
       let filterValue = document.getElementById('filterInput').value.toUpperCase();
-
       // Get names ul
       let ul = document.getElementById('names');
       // Get lis from ul
       let li = ul.querySelectorAll('li.collection-item');
-
       // Loop through collection-item lis
       for(let i = 0;i < li.length;i++){
         let a = li[i].getElementsByTagName('a')[0];
