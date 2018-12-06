c291561c60fd6ad5e78300e121a579ae7a258e42
David Nguyen <david.nnguyen@yahoo.com>
2018-12-06 00:57:32 -0500

    kys

--- script.js
+++ script.js
@@ -1,201 +1,288 @@
 var root_url = "http://comp426.cs.unc.edu:3001/";
 var selected_dept, selected_arri;
 
-$(document).ready(function(){
-  // load AJAX log in information
-  $('.collection.with-header').append('<li class="collection-item"><a href="#">test</a></li>');
-  
-  build_navbar();
-  build_home_interface();
-  console.log('Searching for airport data');
-  $.ajax({
-    type: 'POST',
-    url: root_url + 'sessions/',
-    data: { "user": {
-                    "username": 'b',
-                    "password": 'password'
-                    }
-          },
-    xhrFields: {withCredentials: true},
-    async: true,
-    success: (response) => {
-    	// build_home_interface();
-    	
-      	console.log('POST to sessions was a success');
-    }
-  });
-  
-	$('#date').mouseenter(function(){
-		$('#date').datepicker();
-	});
-
-	$('#choose_btn').on('click', () => {
-		if(selected_arri==selected_dept){
-			alert("cannot fly to same place idiot");
-		} else if(selected_dept!=null && selected_arri!=null){
-			console.log("chosen date valid");
-			build_flight_interface();
-		} else {
-			alert("chosen date invalid");
-		}
-	});
-
-	// departure
-	let deptairportcont = $('.collection.dept-with-header');
-	let arriairportcont = $('.collection.arri-with-header');
-
-	//Populate Search with Aiports
-	$.ajax({
-    type: 'GET',
-    url: root_url + 'airports',
-    xhrFields: {withCredentials: true},
-    success: (response) => {
-    	if(response!=null){
-    	let airports = response;
-    	console.log('airport data accessed');
-    	let testairport = airports[0];
-    	console.log(testairport);
-    	console.log(airports);
-    	for (var i = 0; i < airports.length; i++){
-    		console.log(i);
-    		console.log(airports[i].name);
-    		let airport_name = airports[i].name;
-    		console.log(airport_name);
-    		deptairportcont.append('<li class="collection-item"><a href="#">' + airport_name +'</a></li>');
-    		arriairportcont.append('<li class="collection-item"><a href="#">' + airport_name +'</a></li>');
-
-    		
-	    	}
-	    }
-  }
-  
-
-});
-
-$(".collection.dept-with-header").on("click", ".collection-item", function(e){
-  selected_dept = (e.target.textContent);
-  var input= $('#filterInput');
-  input.val(selected_dept);
-});
-
-$(".collection.arri-with-header").on("click", ".collection-item", function(e) {
-  selected_arri= (e.target.textContent);
-  var input= $('#filterInput2');
-  input.val(selected_arri);
-
-  var keyword = e.target.textContent;
-  console.log(keyword);
-  
-
-        $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
-        {
-            tags: keyword,
-            tagmode: "any",
-            format: "json"
+$(document).ready(() => {
+  $('#reg_btn').on('click', () => {
+    let usern = $('#login_user').val();
+    let pass = $('#login_pass').val();
+    $.ajax(root_url + 'users', {
+      type: 'POST',
+      dataType: 'json',
+      data: {
+        "user": {
+          "username": usern,
+          "password": pass
         },
-        function(data) {
-            var rnd = Math.floor(Math.random() * data.items.length);
-
-            var image_src = data.items[rnd]['media']['m'].replace("_m", "_b");
-
-            $('nav').css('background-image', "url('" + image_src + "')");
-
-    
-		});
-
-
+      },
+      success: () => {
+        build_navbar();
+        build_home_interface();
+      },
+      error: () => {
+        alert("sad");
+      }
     });
+  });
+
+  $('#login_btn').on('click', () => {
+    let usern = $('#login_user').val();
+    let pass = $('#login_pass').val();
+
+    $.ajax(root_url + 'sessions', {
+      type: 'POST',
+      dataType: 'json',
+      data: {
+        "user": {
+          "username": usern,
+          "password": pass
+        },
+      },
+      success: () => {
+        build_navbar();
+        build_home_interface();
+      },
+      error: () => {
+        alert('login error');
+      }
+    });
+
+    $("body").on("click", "#1", function() {
+      build_home_interface();
+    });
+
+    $("body").on("click", "#2", function() {
+      let body = $('body');
+      body.empty();
+      body.append('<h1>Itinerary</h1>');
+      build_navbar();
+    });
+
+    $("body").on("click", "#3", function() {
+      let body = $('body');
+      body.empty();
+      body.append('<h1>Seat</h1>');
+      build_navbar();
+    });
+  });
 });
 
 var build_flight_interface = function() {
-    let body = $('body');
-    //make container but make sure to close container and divs
-    body.append('<div class="container results-container"><div id="wrapper"></div><div id="under">');
-    let rlist = $('<ul class="collection results-collection">');
-    
-    // let arri_id = get_airport_id(selected_arri);
-    let dept_id = get_airport_id(selected_dept).number;
-    console.log(root_url + 'flights?filter[departure_id]=' + dept_id);
-    console.log("final dept_id is: " + get_airport_id(selected_dept).number);
+  let body = $('body');
+  //make container but make sure to close container and divs
+  body.append('<div class="container results-container"><div id="wrapper"></div><div id="under">');
+  let rlist = $('<ul class="collection results-collection">');
 
-    $.ajax({
-		type: 'GET',
-		// url: root_url + 'flights?filter[departure_id]=' + dept_id + '?filter[arrival_id]=' + arri_id,
-		url: root_url + 'flights?filter[departure_id]=' + dept_id,
-		xhrFields: {withCredentials: true},
-		success: (response) => {
-			// console.log(response[0]);
-			let departures = response;
+  // let arri_id = get_airport_id(selected_arri);
+  let dept_id = get_airport_id(selected_dept).number;
+  console.log(root_url + 'flights?filter[departure_id]=' + dept_id);
+  console.log("final dept_id is: " + get_airport_id(selected_dept).number);
 
-			console.log(departures);
-		}
-	});
-	//use LA departure and McCarran arrival
-	// get_airport_name(125570);
-	// get_airport_name(125718);
-	// http://comp426.cs.unc.edu:3001/flights?filter[departure_id]=134872
+  $.ajax({
+    type: 'GET',
+    // url: root_url + 'flights?filter[departure_id]=' + dept_id + '?filter[arrival_id]=' + arri_id,
+    url: root_url + 'flights?filter[departure_id]=' + dept_id,
+    xhrFields: {
+      withCredentials: true
+    },
+    success: (response) => {
+      // console.log(response[0]);
+      let departures = response;
+
+      console.log(departures);
+    }
+  });
+  //use LA departure and McCarran arrival
+  // get_airport_name(125570);
+  // get_airport_name(125718);
+  // http://comp426.cs.unc.edu:3001/flights?filter[departure_id]=134872
 
 }
 
 var get_airport_id = function(airportname) {
-	$.ajax({
-		type: 'GET',
-		url: root_url + 'airports?filter[name]=' + airportname,
-		xhrFields: {withCredentials: true},
-		success: (response) => {
-			// console.log(response[0]);
-			airport_id = response[0];
-			console.log(airport_id);
-			
-		}
-	});
+  $.ajax({
+    type: 'GET',
+    url: root_url + 'airports?filter[name]=' + airportname,
+    xhrFields: {
+      withCredentials: true
+    },
+    success: (response) => {
+      // console.log(response[0]);
+      airport_id = response[0];
+      console.log(airport_id);
+    }
+  });
 }
+
 var get_airport_name = function(some_airport_id) {
-	$.ajax({
-		type: 'GET',
-		url: root_url + 'airports/' + some_airport_id,
-		xhrFields: {withCredentials: true},
-		success: (response) => {
-			// console.log(response[0]);
-			let this_airport_name = response.name;
-			console.log(response);
-		}
-	});
+  $.ajax({
+    type: 'GET',
+    url: root_url + 'airports/' + some_airport_id,
+    xhrFields: {
+      withCredentials: true
+    },
+    success: (response) => {
+      // console.log(response[0]);
+      let this_airport_name = response.name;
+      console.log(response);
+    }
+  });
 }
 
-var change_pass_btn = function(){
-	let body = $('body')
-	body.empty();
-	build_navbar();
+var build_navbar = function() {
+  let body = $('body')
+
+  body.append('<nav><li id="1";><a>Book Flight</a></li><li id="2"><a> Itinerary</a></li><li id="3"><a> Seat</a></li><li  onClick="change_pass_btn()"><a> Change Password</a></li></nav>');
 }
 
-var build_navbar = function(){
-	let body = $('body')
-	body.append('<nav><li onClick="build_home_interface()"><a>Book Flight</a></li><li><a> Itinerary</a></li><li><a> Seat</a></li><li onClick="change_pass_btn()"><a> Change Password</a></li></nav>');
-		
-}
-var build_home_interface = function(){
-	let body = $('body')
 
-	body.empty();
-	build_navbar();
-	body.append('<h1>Flight API Project</h1><div class="container flight-container"><div id="wrapper"><div id="left">Departure: <input type="text" id="filterInput" placeholder="Search names..."><ul id="names" class="collection dept-with-header"></ul></div><div id="middle">Arrival: <input type="text" id="filterInput2"  placeholder="Search names..."><ul id="names2" class="collection arri-with-header" class="left-align"></ul></div><div id="right">Date: <input class="calendar" id="date" placeholder="Select date"></div></div><button onclick= " build_flight_interface"id="choose_btn">Choose Date</button><script src="filterlist.js"></script><script src="filterlist2.js"></script>');
-		
+var build_home_interface = function() {
+  var selected_dept, selected_arri;
+  let body = $('body')
+  body.empty();
+  body.append('<h1>Flight API Project</h1>');
+  build_navbar();
+  body.append('<br><div class="container-flight-container"><div id="wrapper"><div id="left">Departure: <input type="text" id="filterInput" placeholder="Search names..."><ul id="names" class="collection dept-with-header"></ul></div><div id="middle">Arrival: <input type="text" id="filterInput2"  placeholder="Search names..."><ul id="names2" class="collection arri-with-header" class="left-align"></ul></div><div id="right">Date: <input class="calendar" id="date" placeholder="Select date"></div><button onclick= " build_flight_interface"id="choose_btn">Find Flights</button></div><script src="filterlist.js"></script><script src="filterlist2.js"></script>');
+  $('#date').mouseenter(function() {
+    $('#date').datepicker();
+  });
+
+  $('#choose_btn').on('click', () => {
+    if (selected_arri == selected_dept) {
+      alert("cannot fly to same place idiot");
+    } else if (selected_dept != null && selected_arri != null) {
+      console.log("chosen date valid");
+      build_flight_interface();
+    } else {
+      alert("chosen date invalid");
+    }
+  });
+
+  // departure
+  let deptairportcont = $('.collection.dept-with-header');
+  let arriairportcont = $('.collection.arri-with-header');
+
+  //Populate Search with Aiports
+  $.ajax({
+    type: 'GET',
+    url: root_url + 'airports',
+    xhrFields: {
+      withCredentials: true
+    },
+    success: (response) => {
+      if (response != null) {
+        let airports = response;
+        console.log('airport data accessed');
+        let testairport = airports[0];
+        console.log(testairport);
+        console.log(airports);
+        for (var i = 0; i < airports.length; i++) {
+          console.log(i);
+          console.log(airports[i].name);
+          let airport_name = airports[i].name;
+          console.log(airport_name);
+          deptairportcont.append('<li class="collection-item"><a href="#">' + airport_name + '</a></li>');
+          arriairportcont.append('<li class="collection-item"><a href="#">' + airport_name + '</a></li>');
+        }
+      }
+    }
+  });
+
+  $(".collection.arri-with-header").on("click", ".collection-item", function(e) {
+    selected_arri = (e.target.textContent);
+    console.log('penis');
+    var input = $('#filterInput2');
+    input.val(selected_arri);
+    var keyword = e.target.textContent;
+    console.log(keyword);
+
+    $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?", {
+        tags: keyword,
+        tagmode: "any",
+        format: "json"
+      },
+      function(data) {
+        var rnd = Math.floor(Math.random() * data.items.length);
+        var image_src = data.items[rnd]['media']['m'].replace("_m", "_b");
+        $('nav').css('background-image', "url('" + image_src + "')");
+
+      });
+  });
+
+  $(".collection.dept-with-header").on("click", ".collection-item", function(e) {
+    selected_dept = (e.target.textContent);
+    var input = $('#filterInput');
+    input.val(selected_dept);
+  });
 }
+
+$(".collection.arri-with-header").on("click", ".collection-item", function(e) {
+  selected_arri = (e.target.textContent);
+  console.log('penis');
+  var input = $('#filterInput2');
+  input.val(selected_arri);
+
+  var keyword = e.target.textContent;
+  console.log(keyword);
+
+  $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?", {
+      tags: keyword,
+      tagmode: "any",
+      format: "json"
+    },
+    function(data) {
+      var rnd = Math.floor(Math.random() * data.items.length);
+      var image_src = data.items[rnd]['media']['m'].replace("_m", "_b");
+      $('nav').css('background-image', "url('" + image_src + "')");
+    });
+});
+
+$(".collection.dept-with-header").on("click", ".collection-item", function(e) {
+  selected_dept = (e.target.textContent);
+  var input = $('#filterInput');
+  input.val(selected_dept);
+});
+
+var change_pass_btn = function() {
+  let body = $('body')
+  body.empty();
+  body.append('<h1>Change Password</h1>');
+  build_navbar();
+  body.append('<div id="Newpass_div">User: <input type="text" id="login_user" value="b"><br>Password: <input type="text" id="login_pass" value="Old Password"><br>New Password: <input type="text" id="login_Newpass" value="New password"><br></br><button id="newPass_btn"> Set New Password</button></div>');
+
+  $('#newPass_btn').on('click', () => {
+    let usern = $('#login_user').val();
+    let pass = $('#login_pass').val();
+    let newPass = $('#login_Newpass').val();
+
+    $.ajax(root_url + 'passwords', {
+      type: 'PUT',
+      dataType: 'json',
+      data: {
+        "user": {
+          "username": usern,
+          "old_password": pass,
+          "new_password": newPass
+        },
+      },
+      success: () => {
+        alert("nice")
+      },
+      error: () => {
+        alert("sad");
+      }
+    });
+  });
+}
+
+
+
+
+
 // var queryFlights = function() {
 // 	let body = $('body');
 
 // 	if(selected_arri=null) AND (elected_dept!=null):
 // 		body.empty();
-	
+
 
 // };
-
-
-
-
-
-
-
-
-
