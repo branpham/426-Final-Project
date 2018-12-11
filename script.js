83b49adfaebfb4a862c36440a2cdab6eb6f43c0f
branpham <branpham@branpham-pc.wireless-1x.unc.edu>
2018-12-11 15:55:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ var root_url = "http://comp426.cs.unc.edu:3001/";
 var selected_dept, selected_arri;
 cnt = 0; 
 var itineraryTable =  $(`<table id="itinerary"><tr>
-  <th onclick="sortTable(6)" style="cursor:pointer">Name</th>
-  <th onclick="sortTable(7)" style="cursor:pointer">Logistics</th>
-  <th onclick="sortTable(8)" style="cursor:pointer">Flight</th>
-  <th onclick="sortTable(9)" style="cursor:pointer">Confirmation Number</th>
+  <th onclick="sortTable2(0)" style="cursor:pointer">Name</th>
+  <th onclick="sortTable2(1)" style="cursor:pointer">Logistics</th>
+  <th onclick="sortTable2(2)" style="cursor:pointer">Flight</th>
+  <th onclick="sortTable2(3)" style="cursor:pointer">Confirmation Number</th>
   </tr></table>`);
 
 $(document).ready(() => {
@@ -618,6 +618,60 @@ conf = new Array('C42JO', 'S42KO', 'Z20SM', 'Q43JO', 'S10JO', 'L20KD');
 function sortTable(n) {
   var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
   table = document.getElementById("flights");
+  switching = true;
+  // Set the sorting direction to ascending:
+  dir = "asc";
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /* Loop through all table rows (except the
+    first, which contains table headers): */
+    for (i = 1; i < (rows.length - 1); i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Get the two elements you want to compare,
+      one from current row and one from the next: */
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      /* Check if the two rows should switch place,
+      based on the direction, asc or desc: */
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      // Each time a switch is done, increase this count by 1:
+      switchcount ++;
+    } else {
+      /* If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again. */
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
+function sortTable2(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("itinerary");
   switching = true;
   // Set the sorting direction to ascending:
   dir = "asc";
